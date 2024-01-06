e9f67c9cd495b4439bbc2a0edb871fea54f0bad5
Jim Whiteman <jimtron9000@gmail.com>
2024-01-06 07:59:25 -0700

    add base64
    https://nachtimwald.com/2017/11/18/base64-encode-and-decode-in-c/

--- 1/c/1.c
+++ 1/c/1.c
@@ -3,18 +3,23 @@
 #include <string.h>
 
 #include "../../lib/decodehex.h"
+#include "../../lib/base64.h"
 
-// TODO: try to call from chicken
 // TODO: use base64
+// TODO: try to call chicken from c
 
 int
 main()
 {
-  char *input  = "49276d206b696c6c696e6720796f757220627261696e206"\
-                 "c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-  char *output = decode_hex_string(input);
+  char *input     = "49276d206b696c6c696e6720796f757220627261696e206"\
+                    "c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
-  printf("\n****** TEST OUTPUT ******: %s\n\n", output);
+  char *text      = decode_hex_string(input);
+  char *enc_text  = b64_encode((const unsigned char *)text, strlen(text));
 
-  free(output);
+  printf("\n****** TEST OUTPUT ******: %s\n\n", text);
+  printf("\n****** TEST OUTPUT ******: %s\n\n", enc_text);
+
+  free(text);
+  free(enc_text);
 }
