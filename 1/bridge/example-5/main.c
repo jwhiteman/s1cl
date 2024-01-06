987ea5576f482a6b861fde414c0541df0ea8dc5b
Jim Whiteman <jimtron9000@gmail.com>
2024-01-06 07:29:19 -0700

    bridge example-5

--- 1/bridge/example-5/main.c
+++ 1/bridge/example-5/main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include "../../lib/decodehex.h"
-
-// TODO: try to call from chicken
-// TODO: use base64
+#include "decodehex.h"
 
 int
 main()
 {
   char *input  = "49276d206b696c6c696e6720796f757220627261696e206"\
                  "c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
   char *output = decode_hex_string(input);
 
-  printf("\n****** TEST OUTPUT ******: %s\n\n", output);
+  printf("the result is: %s\n", output);
 
   free(output);
+  // free(output); // to induce "pointer being freed was not allocated" error
 }
