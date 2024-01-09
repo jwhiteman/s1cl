612344b6fa4bafe697543181ed2e0cf235ddb02c
Jim Whiteman <jimtron9000@gmail.com>
2024-01-09 09:47:02 -0700

    say hello to my little friend: cl format

--- 2/2.scm
+++ 2/2.scm
@@ -1,6 +1,10 @@
+;; chicken-install format => http://wiki.call-cc.org/eggref/5/format
+;; note: (chicken format) and srfi-28 both implement "format" functions
+
 (import
   (only regex string-split-fields)
-  (only (chicken bitwise) bitwise-xor))
+  (only (chicken bitwise) bitwise-xor)
+  (only format format))
 
 (define input "1c0111001f010100061a024b53535009181c")
 (define key "686974207468652062756c6c277320657965")
@@ -10,12 +14,10 @@
 
 (define (fixed-xor input key)
   (map (lambda (l r)
-         (number->string
-           (bitwise-xor (string->number l 16)
-                        (string->number r 16))
-           16))
+         (bitwise-xor (string->number l 16)
+                      (string->number r 16)))
        (hex-list input)
        (hex-list key)))
 
-(print (apply string-append (fixed-xor input key)))
+(print (format "~{~x~}" (fixed-xor input key)))
 ;; "746865206b696420646f6e277420706c6179"
