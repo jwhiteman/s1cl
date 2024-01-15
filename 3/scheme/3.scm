824ccaaa78cf11f12de7b7196843a3d0215b856f
Jim Whiteman <jimtron9000@gmail.com>
2024-01-14 22:15:23 -0700

    ok

--- 3/scheme/3.scm
+++ 3/scheme/3.scm
@@ -7,7 +7,8 @@
   (only regex string-split-fields)
   (only (chicken bitwise) bitwise-xor))
 
-(define input "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+(define input
+  "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
 (define (score str)
   (length (string-split-fields "(?i)[etaoin shrdlu]" str)))
