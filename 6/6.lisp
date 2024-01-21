4d841cd7603b7615cb95c35daf8622143b0b4997
Jim Whiteman <jimtron9000@gmail.com>
2024-01-21 09:30:47 -0700

    imperative hamming in lisp

--- 6/6.lisp
+++ 6/6.lisp
@@ -1,28 +1,26 @@
 (ql:quickload :cl-base64)
 
 (defun to-bytes (str)
-  (map 'list #'char-code str))
+  (map 'vector #'char-code str))
 
 (defconstant input-bytes
              (with-open-file (instream "6.txt")
                (to-bytes
                  (cl-base64:base64-stream-to-string instream))))
 
-(defun byte-distance (byte1 byte2)
-  (labels ((H (xor-result idx num-diff-bits)
-              (if (zerop idx)
-                num-diff-bits
-                (H (ash xor-result -1)
-                   (1- idx)
-                   (+ num-diff-bits (logand xor-result 1))))))
-    (H (logxor byte1 byte2) 8 0)))
-
+;; meh...the c is actually nicer for this type of style
 (defun hamming-distance (str1 str2)
-  (labels ((H (bytes1 bytes2 count)
-              (if (null bytes1)
-                count
-                (H (cdr bytes1)
-                   (cdr bytes2)
-                   (+ count (byte-distance (car bytes1)
-                                           (car bytes2)))))))
-    (H (to-bytes str1) (to-bytes str2) 0)))
+  (let* ((bytes1 (to-bytes str1))
+         (bytes2 (to-bytes str2))
+         (len (length bytes1))
+         (count 0))
+    (do ((idx 0 (1+ idx)))
+      ((= idx len) count)
+      (let ((xor (logxor (aref bytes1 idx)
+                         (aref bytes2 idx))))
+        (do ((bidx 0 (1+ bidx)))
+          ((= bidx 8))
+          (setf count (+ count (logand xor 1))
+                xor (ash xor -1)))))))
+
+(hamming-distance "this is a test" "wokka wokka!!!")
