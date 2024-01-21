92723abfb92f2e6ec95772d97ac80fa34ccaa1c0
Jim Whiteman <jimtron9000@gmail.com>
2024-01-21 08:27:45 -0700

    Switch label fun name

--- 6/6.lisp
+++ 6/6.lisp
@@ -9,20 +9,20 @@
                  (cl-base64:base64-stream-to-string instream))))
 
 (defun byte-distance (byte1 byte2)
-  (labels ((accumulate (xor-result idx num-diff-bits)
-                       (if (zerop idx)
-                         num-diff-bits
-                         (accumulate (ash xor-result -1)
-                                     (1- idx)
-                                     (+ num-diff-bits (logand xor-result 1))))))
-    (accumulate (logxor byte1 byte2) 8 0)))
+  (labels ((H (xor-result idx num-diff-bits)
+              (if (zerop idx)
+                num-diff-bits
+                (H (ash xor-result -1)
+                   (1- idx)
+                   (+ num-diff-bits (logand xor-result 1))))))
+    (H (logxor byte1 byte2) 8 0)))
 
 (defun hamming-distance (str1 str2)
-  (labels ((accumulate (bytes1 bytes2 count)
-                       (if (null bytes1)
-                         count
-                         (accumulate (cdr bytes1)
-                                     (cdr bytes2)
-                                     (+ count (byte-distance (car bytes1)
-                                                             (car bytes2)))))))
-    (accumulate (to-bytes str1) (to-bytes str2) 0)))
+  (labels ((H (bytes1 bytes2 count)
+              (if (null bytes1)
+                count
+                (H (cdr bytes1)
+                   (cdr bytes2)
+                   (+ count (byte-distance (car bytes1)
+                                           (car bytes2)))))))
+    (H (to-bytes str1) (to-bytes str2) 0)))
