d8791bf737a245f7a3e69aae355e0a2556a2f3d8
Jim Whiteman <jimtron9000@gmail.com>
2024-01-22 09:56:38 -0700

    find keysize cl - ugly as hell

--- 6/6.lisp
+++ 6/6.lisp
@@ -17,7 +17,7 @@
                    (+ num-diff-bits (logand xor-result 1))))))
     (H (logxor byte1 byte2) 8 0)))
 
-(defun hamming-distance (str1 str2)
+(defun hamming-distance (bytes1 bytes2)
   (labels ((H (bytes1 bytes2 count)
               (if (null bytes1)
                 count
@@ -25,4 +25,41 @@
                    (cdr bytes2)
                    (+ count (byte-distance (car bytes1)
                                            (car bytes2)))))))
-    (H (to-bytes str1) (to-bytes str2) 0)))
+    (H bytes1 bytes2 0)))
+
+(defun normalized-hamming-distance (bytes1 bytes2)
+  (/ (hamming-distance bytes1 bytes2)
+     (length bytes1)))
+
+(defun in-blocks (input block-length num-blocks)
+  (labels ((H (idx acc)
+              (if (= idx num-blocks)
+                (reverse acc)
+                (H (1+ idx)
+                   (cons (subseq input (* block-length idx)
+                                 (* block-length (1+ idx)))
+                         acc)))))
+    (H 0 '())))
+
+(defun avg-hamming-distance (blocks)
+  (labels ((H (prev lob idx acc)
+              (if (null lob)
+                (/ (reduce #'+ acc) idx)
+                (let* ((cur (car lob))
+                       (hd (normalized-hamming-distance prev cur)))
+                  (H cur
+                     (cdr lob)
+                     (1+ idx)
+                     (cons hd acc))))))
+    (H (car blocks) (cdr blocks) 0 '())))
+
+(defun min-sort (res)
+  (sort res #'(lambda (a b) (< (cdr a) (cdr b)))))
+
+(let ((acc))
+  (do ((i 2 (1+ i)))
+    ((> i 40) (min-sort acc))
+    (let ((res
+            (avg-hamming-distance (in-blocks input-bytes i 12))))
+      (setf acc
+            (cons (cons i res) acc)))))
